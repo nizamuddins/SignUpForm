416733ca2a1b7f0d6cf71ba3f9203bb164aecfcf
nizamuddins <uddinmdnizam61@gmail.com>
2022-06-01 13:39:48 +0530

    project

--- index1.js
+++ index1.js
@@ -8,50 +8,50 @@ const span = document.querySelector(".li .li-span")
 
 submit.addEventListener("submit", (e) => {
     e.preventDefault();
-// oneWay
-    const newLi = document.createElement("li");
-    const newText = document.createTextNode(input.value);
-    const newSpan = document.createElement("span");
-     newSpan.append(newText);
-     newSpan.classList.add("li-span")   
-    newLi.append(newSpan);
-    console.log(newLi)
+// // oneWay
+//     const newLi = document.createElement("li");
+//     const newText = document.createTextNode(input.value);
+//     const newSpan = document.createElement("span");
+//      newSpan.append(newText);
+//      newSpan.classList.add("li-span")   
+//     newLi.append(newSpan);
+//     console.log(newLi)
 
-    const div = document.createElement("div")
-    div.classList.add("form-list")
+//     const div = document.createElement("div")
+//     div.classList.add("form-list")
 
-    const button1 = document.createElement("button");
-    button1.textContent = "Done";
-    button1.classList.add("list-btn")
-    button1.classList.add("done")
-    const button2 = document.createElement("button");
-    button2.textContent = "Remove";
-    button2.classList.add("list-btn");
-    button2.classList.add("remove");
+//     const button1 = document.createElement("button");
+//     button1.textContent = "Done";
+//     button1.classList.add("list-btn")
+//     button1.classList.add("done")
+//     const button2 = document.createElement("button");
+//     button2.textContent = "Remove";
+//     button2.classList.add("list-btn");
+//     button2.classList.add("remove");
 
-    div.append(button1);
-    div.append(button2);
+//     div.append(button1);
+//     div.append(button2);
 
-    newLi.append(div);
-    if(input.value!== ""){
-    todoLi.append(newLi);
-
-    }
-
-// secondWay
-//    const newLi = document.createElement("li");
-
-// const innerhtml = `<span class="li-span">${input.value}</span>
-//     <div class="form-list">
-//         <button class="list-btn done">Done</button>
-//         <button class="list-btn remove">Remove</button>
-//     </div>`
-// newLi.innerHTML = innerhtml;
-// if (input.value !== "") {
-
+//     newLi.append(div);
+//     if(input.value!== ""){
 //     todoLi.append(newLi);
 
-// }
+//     }
+
+secondWay
+   const newLi = document.createElement("li");
+
+const innerhtml = `<span class="li-span">${input.value}</span>
+    <div class="form-list">
+        <button class="list-btn done">Done</button>
+        <button class="list-btn remove">Remove</button>
+    </div>`
+newLi.innerHTML = innerhtml;
+if (input.value !== "") {
+
+    todoLi.append(newLi);
+
+}
 
 input.value = "";
 
